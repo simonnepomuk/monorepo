2e146f76cc879f079857c03d5923ed7cfefdb8a3
wagnesi <simon.wagner@iteratec.com>
2022-11-01 00:17:14 +0100

    chore: fix formatting

--- apps/test-app/svelte.config.js
+++ apps/test-app/svelte.config.js
@@ -10,8 +10,8 @@ const config = {
   kit: {
     adapter: adapter({ outDir: '../../dist/apps/test-app' }),
     csrf: {
-      checkOrigin: false
-    }
+      checkOrigin: false,
+    },
   },
 };
 
