6286e6562e0a32a6680f4b8adc8c70c06f346565
wagnesi <simon.wagner@iteratec.com>
2022-10-31 23:35:47 +0100

    ci: fix coverage reports

--- libs/sveltekit-adapter-firebase/vitest.config.ts
+++ libs/sveltekit-adapter-firebase/vitest.config.ts
@@ -3,4 +3,11 @@ import baseConfig from '../../vitest.config';
 
 export default mergeConfig(baseConfig, {
   plugins: [],
+  test: {
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      provider: 'c8',
+      reportsDirectory: '../../coverage/sveltekit-adapter-firebase'
+    },
+  }
 });
