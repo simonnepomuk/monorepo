ab75716b1246d76e7df749a53da159a955fa2356
wagnesi <simon.wagner@iteratec.com>
2022-11-01 01:14:21 +0100

    test: dont build project in playwright.config.ts command

--- apps/test-app/playwright.config.ts
+++ apps/test-app/playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'nx build test-app && nx preview test-app',
+    command: 'nx preview test-app',
     port: 5000,
     timeout: 30000,
   },
