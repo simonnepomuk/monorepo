ff9cd3e53c6be65d8c9c57cb43ce823492637853
wagnesi <simon.wagner@iteratec.com>
2022-11-01 01:21:55 +0100

    ci: fix playwright config

--- apps/test-app/playwright.config.ts
+++ apps/test-app/playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'nx preview test-app',
+    command: 'npx nx preview test-app',
     port: 5000,
     timeout: 30000,
   },
