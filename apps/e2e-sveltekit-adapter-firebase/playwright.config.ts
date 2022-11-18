5ebb29739cefc604e35ac1ce17f3fb4956d0155f
wagnesi <simon.n.wagner@gmail.com>
2022-11-18 13:02:12 +0100

    chore: fix flaky e2e tests

--- apps/e2e-sveltekit-adapter-firebase/playwright.config.ts
+++ apps/e2e-sveltekit-adapter-firebase/playwright.config.ts
@@ -3,8 +3,13 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   webServer: {
     command: 'npx nx preview e2e-sveltekit-adapter-firebase',
-    port: 5000,
-    timeout: 90000,
+    //we abuse the sverdle endpoint as health endpoint for the webserver
+    url: 'http://localhost:5001/sveltekit-adapter/us-central1/handler/sverdle',
+    timeout: 60 * 1000,
+    reuseExistingServer: false,
+  },
+  use: {
+    baseURL: 'http://localhost:5000/',
   },
 };
 
