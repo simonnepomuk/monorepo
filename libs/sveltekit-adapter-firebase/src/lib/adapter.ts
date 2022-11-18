33ee705c1c2fe6baf76439d3e75d9c359d0fb8fe
wagnesi <simon.n.wagner@gmail.com>
2022-11-18 09:14:52 +0100

    fix: install dependencies in generated directory

--- libs/sveltekit-adapter-firebase/src/lib/adapter.ts
+++ libs/sveltekit-adapter-firebase/src/lib/adapter.ts
@@ -3,6 +3,7 @@ import { join } from 'path';
 import { fileURLToPath } from 'url';
 import { Builder } from '@sveltejs/kit';
 import { HttpsOptions } from 'firebase-functions/lib/v2/providers/https';
+import { spawnSync } from 'child_process';
 
 const distPath = fileURLToPath(new URL('.', import.meta.url).href);
 
@@ -62,6 +63,14 @@ export default function (options?: AdapterOptions) {
       builder.log.info('Generating production package.json for Firebase...');
 
       generateProductionPackageJson({ outDir, nodeVersion });
+
+      console.log(
+        'Installing dependencies in functions directory. This might take a while...'
+      );
+      spawnSync('npm', ['install'], {
+        cwd: outDir,
+        stdio: 'inherit',
+      });
     },
   };
 }
